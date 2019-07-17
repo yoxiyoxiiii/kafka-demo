8796db0651996b54fc464eef320e3bb4595a3f7c
846814595@qq.com <846814595@qq.com>
2019-07-17 20:27:32 +0800

    添加注解

--- src/main/java/com/example/kafkademo/hello/CustomerHelloMsg.java
+++ src/main/java/com/example/kafkademo/hello/CustomerHelloMsg.java
@@ -15,8 +15,14 @@ public class CustomerHelloMsg {
 
     public static void rev() {
         Properties properties = new Properties();
+        //配置kafka 服务器地址
         properties.put("bootstrap.servers","127.0.0.1:9092");
+        //设置消费者的 groupId，同一个分组的消费者只有其中一个消费者能消费到消息
         properties.put("group.id","hello");
+        //设置消息 拉取策略
+        //earliest: 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费
+        // latest: 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，消费新产生的该分区下的数据
+        // none ：topic各分区都存在已提交的offset时，从offset后开始消费；只要有一个分区不存在已提交的offset，则抛出异常
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
         //enable.auto.commit配置项指定了提交offset的方式为自动提交
         properties.put("enable.auto.commit", "true");
